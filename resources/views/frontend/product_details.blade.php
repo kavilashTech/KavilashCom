d46900958b3fa5bcaf476c47e264dc18e37bfa19
Sampath <kavilashtech@gmail.com>
2022-10-05 16:05:11 +0530

    udpated returnable image

--- resources/views/frontend/product_details.blade.php
+++ resources/views/frontend/product_details.blade.php
@@ -451,7 +451,7 @@
                             @if($detailedProduct->returnable == 1)
                             <!-- <img src="{{ static_asset('assets/img/placeholder.jpg') }}https://images-na.ssl-images-amazon.com/images/G/31/A2I-Convert/mobile/IconFarm/icon-returns._CB484059092_.png"> -->
                             <img src="{{ static_asset('assets/img/return_icon_green.jpg') }}">
-                            &nbsp;Returnable
+                            &nbsp;&nbsp;Returnable
                             @endif
                         </div>
                     </div>
