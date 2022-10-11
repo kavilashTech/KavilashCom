393b23f4942c402fecd4c683a15e2a05a096c560
parasshah438 <parasshah438@gmail.com>
2022-10-12 01:00:56 +0530

    category changes and searchbar text

--- resources/views/frontend/partials/category_elements.blade.php
+++ resources/views/frontend/partials/category_elements.blade.php
@@ -1,12 +1,13 @@
-<div class="card-columns">
+<div class="">
+    
     @foreach (\App\Utility\CategoryUtility::get_immediate_children_ids($category->id) as $key => $first_level_id)
-        <div class="card shadow-none border-0">
-            <ul class="list-unstyled mb-3">
-                <li class="fw-600 border-bottom pb-2 mb-3">
+        <div class="">
+            <ul class="list-unstyled categories no-scrollbar py-2 mb-0 text-left">
+                <li class="ml-3 mt-2">
                     <a class="text-reset" href="{{ route('products.category', \App\Models\Category::find($first_level_id)->slug) }}">{{ \App\Models\Category::find($first_level_id)->getTranslation('name') }}</a>
                 </li>
                 @foreach (\App\Utility\CategoryUtility::get_immediate_children_ids($first_level_id) as $key => $second_level_id)
-                    <li class="mb-2">
+                    <li class="ml-5 mt-2">
                         <a class="text-reset" href="{{ route('products.category', \App\Models\Category::find($second_level_id)->slug) }}">{{ \App\Models\Category::find($second_level_id)->getTranslation('name') }}</a>
                     </li>
                 @endforeach
--- resources/views/frontend/partials/category_menu.blade.php
+++ resources/views/frontend/partials/category_menu.blade.php
@@ -5,6 +5,7 @@
             <span class="d-none d-lg-inline-block">{{ translate('See All') }} ></span>
         </a>
     </div>
+    
     <ul class="list-unstyled categories no-scrollbar py-2 mb-0 text-left">
         @foreach (\App\Models\Category::where('level', 0)->orderBy('order_level', 'desc')->get()->take(11) as $key => $category)
             <li class="category-nav-element" data-id="{{ $category->id }}">
@@ -20,10 +21,8 @@
                     <span class="cat-name">{{ $category->getTranslation('name') }}</span>
                 </a>
                 @if(count(\App\Utility\CategoryUtility::get_immediate_children_ids($category->id))>0)
-                    <div class="sub-cat-menu c-scrollbar-light rounded shadow-lg p-4">
-                        <div class="c-preloader text-center absolute-center">
-                            <i class="las la-spinner la-spin la-3x opacity-70"></i>
-                        </div>
+                    <div class="sub-cat-menu">
+                        
                     </div>
                 @endif
             </li>
