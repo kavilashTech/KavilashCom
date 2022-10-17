ae8491ad3153d526319cd6e4da91aff942215d23
parasshah438 <parasshah438@gmail.com>
2022-10-17 22:49:57 +0530

    change category menu page

--- resources/views/frontend/partials/category_menu.blade.php
+++ resources/views/frontend/partials/category_menu.blade.php
@@ -1,11 +1,7 @@
 <div class="aiz-category-menu bg-white rounded @if(Route::currentRouteName() == 'home') shadow-sm" @else shadow-lg" id="category-sidebar" @endif>
     <div class="p-3 bg-soft-primary d-none d-lg-block rounded-top all-category position-relative text-left">
         <span class="fw-600 fs-16 mr-3">{{ translate('Categories') }}</span>
-        <a href="{{ route('categories.all') }}" class="text-reset">
-            <span class="d-none d-lg-inline-block">{{ translate('See All') }} ></span>
-        </a>
     </div>
-    
     <ul class="list-unstyled categories no-scrollbar py-2 mb-0 text-left">
         @foreach (\App\Models\Category::where('level', 0)->orderBy('order_level', 'desc')->get()->take(11) as $key => $category)
             <li class="category-nav-element" data-id="{{ $category->id }}">
@@ -27,5 +23,44 @@
                 @endif
             </li>
         @endforeach
+        <li>
+            <a href="#" data-toggle="modal" data-target="#categoryModel" class="text-truncate text-reset py-2 px-3 d-block ml-2">See All Categories</a>
+        </li>
+
     </ul>
+</div>
+
+@php
+$all_category = \App\Models\Category::where('level', 0)->get();
+@endphp
+<div class="modal modal-fullscreen-xl" id="categoryModel" tabindex="-1" role="dialog" aria-hidden="true">
+  <div class="modal-dialog modal-xl" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <ul class="list-unstyled mb-0 row gutters-5">
+            @foreach($all_category as $category)
+            <li class="minw-0 col-4 col-md mt-3">
+                <a href="http://localhost/kavilashcom/category/fresh-vegetable" class="d-block rounded bg-white p-2 text-reset shadow-sm">
+                    <img
+                        class="cat-image lazyload mr-2 opacity-60"
+                        src="{{ static_asset('assets/img/placeholder.jpg') }}"
+                        data-src="{{ uploaded_asset($category->banner) }}"
+                        width="16"
+                        alt="{{ $category->getTranslation('name') }}"
+                        onerror="this.onerror=null;this.src='{{ static_asset('assets/img/placeholder.jpg') }}';"
+                    >
+                    <div class="text-truncate fs-12 fw-600 mt-2 opacity-70">{{ $category->getTranslation('name') }}</div>
+                </a>
+            </li>
+            @endforeach                                     
+        </ul>
+      </div>
+    </div>
+  </div>
 </div>
