8e12f83e53a33563782245cacad343e6cbbbb40b
carunyadhav <carunyadhav@enoahisolution.com>
2022-10-22 21:56:30 +0530

    0000104: Static pages to be added fixes
    0000104: Static pages to be added fixes

--- resources/views/frontend/inc/nav.blade.php
+++ resources/views/frontend/inc/nav.blade.php
@@ -260,13 +260,15 @@
         </div>
         
     </div>
+
+
     @if ( get_setting('header_menu_labels') !=  null )
         <div class="bg-white border-top border-gray-200 py-1">
             <div class="container">
                 <ul class="list-inline mb-0 pl-0 mobile-hor-swipe text-center">
                     @foreach (json_decode( get_setting('header_menu_labels'), true) as $key => $value)
                     <li class="list-inline-item mr-0">
-                        <a href="{{ json_decode( get_setting('header_menu_links'), true)[$key] }}" class="opacity-60 fs-14 px-3 py-2 d-inline-block fw-600 hov-opacity-100 text-reset">
+                        <a href="{{ env('APP_URL') }}{{ json_decode( get_setting('header_menu_links'), true)[$key] }}" class="opacity-60 fs-14 px-3 py-2 d-inline-block fw-600 hov-opacity-100 text-reset">
                             {{ translate($value) }}
                         </a>
                     </li>
