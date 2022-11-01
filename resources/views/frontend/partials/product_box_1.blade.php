66285d73ffc8847c8891efc195ca1619877cc89f
ankur thakrar <ankurthakrar404@gmail.com>
2022-11-01 21:31:38 +0530

    0000144: Product UI Changes in Frontend

--- resources/views/frontend/partials/product_box_1.blade.php
+++ resources/views/frontend/partials/product_box_1.blade.php
@@ -25,13 +25,13 @@
         @endif
         <div class="absolute-top-right aiz-p-hov-icon">
             <a href="javascript:void(0)" onclick="addToWishList({{ $product->id }})" data-toggle="tooltip" data-title="{{ translate('Add to wishlist') }}" data-placement="left">
-                <i class="la la-heart-o"></i>
+                <i class="la la-heart-o la-2x pt-1"></i>
             </a>
             <!-- <a href="javascript:void(0)" onclick="addToCompare({{ $product->id }})" data-toggle="tooltip" data-title="{{ translate('Add to compare') }}" data-placement="left">
                 <i class="las la-sync"></i>
             </a> -->
             <a href="javascript:void(0)" onclick="showAddToCartModal({{ $product->id }})" data-toggle="tooltip" data-title="{{ translate('Add to cart') }}" data-placement="left">
-                <i class="las la-shopping-cart"></i>
+                <i class="las la-shopping-cart la-2x pt-1"></i>
             </a>
         </div>
     </div>
@@ -40,7 +40,7 @@
             @if(home_base_price($product) != home_discounted_base_price($product))
                 <del class="fw-600 opacity-50 mr-1">{{ home_base_price($product) }}</del>
             @endif
-            <span class="fw-700 text-primary">{{ home_discounted_base_price($product) }}</span>
+            <span class="fw-700 text-primary float-right">{{ home_discounted_base_price($product) }}</span>
         </div>
         <div class="rating rating-sm mt-1">
             {{ renderStarRating($product->rating) }}
@@ -49,9 +49,10 @@
             <a href="{{ $product_url }}" class="d-block text-reset">{{  $product->getTranslation('name')  }}</a>
         </h3>
         @if (addon_is_activated('club_point'))
-            <div class="rounded px-2 mt-2 bg-soft-primary border-soft-primary border">
+            <!-- <div class="rounded px-2 mt-2 bg-soft-primary border-soft-primary border"> -->
+            <div class="rounded px-2 mt-2">
                 {{ translate('Club Point') }}:
-                <span class="fw-700 float-right">{{ $product->earn_point }}</span>
+                <span class="fw-700">{{ $product->earn_point }}</span>
             </div>
         @endif
     </div>
